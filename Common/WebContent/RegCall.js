c899373bc6c497e376ad256edea8ce9c6d18f6f1
tonsr <ak48159@qq.com>
2015-11-17 10:48:39 +0800

    Update RegCall.js

--- Common/WebContent/RegCall.js
+++ Common/WebContent/RegCall.js
@@ -1,6 +1,19 @@
 /**
- * 循环调用 类似java中定时器
- */
+ * 定时器封装，
+ * @param upfun 定时执行的函数代码块
+ * @param timer 定时时间周期 毫秒数
+ * @param arg 	定时函数所需的参数数据 格式为json
+ * 
+ * @author 唐明豪
+ * @date   2015-09-06 
+ * 
+ * @functions 
+ * 			1.构造函数返回当前对象。
+ * 			2.stop()  停止定时器执行。
+ * 			3 pause() 暂停定时器执行，在下次时钟周期时开始暂停。
+ * 			4.play()  继续定时器任务，在下次到达时钟周期时继续执行。 
+ * 			5 start() 启动定时器任务。立即执行定时任务。
+ * */
 var RegCall = function(upfun,timer,arg){
 	this.regCallFlag = false;
 	this.interval = undefined;

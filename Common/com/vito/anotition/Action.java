cf9a9e6ad8c7e638bce7168aab5de560b378ff97
tonsr <ak48159@qq.com>
2016-07-14 18:36:25 +0800

    Create Action.java

---
+++ Common/com/vito/anotition/Action.java
@@ -0,0 +1,14 @@
+package com.vito.act.anotition;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+public @interface Action {
+	public String value() default "";
+}
